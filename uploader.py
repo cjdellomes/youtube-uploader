fda30edf20c29d23573049890002918695fba1b9
cjdellomes <cjdellomes@gmail.com>
2020-11-02 00:03:26 -0800

    Break up the main function into separate functions

--- uploader.py
+++ uploader.py
@@ -19,43 +19,49 @@ import googleapiclient.errors
 
 from googleapiclient.http import MediaFileUpload
 
-scopes = ["https://www.googleapis.com/auth/youtube.upload"]
+API_SERVICE_SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
+CLIENT_SECRETS_FILE = "client_secret.json"
+FILE = "test.mp4"
 
-
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "client_secret.json"
-
-    # Get credentials and create an API client
+def get_credentials(client_secrets_file, scopes):
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         client_secrets_file, scopes)
     credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+    return credentials
 
-    response = None
-    request = youtube.videos().insert(
+def get_youtube_video_insert_request(api_client, file, description, title):
+    request = api_client.videos().insert(
         part="snippet,status",
         body={
             "snippet": {
                 "categoryId": "22",
-                "description": "Description of uploaded video.",
-                "title": "Test video upload."
+                "description": description,
+                "title": title
             },
             "status": {
                 "privacyStatus": "unlisted"
             }
         },
 
-        # TODO: For this request to work, you must replace "YOUR_FILE"
-        #       with a pointer to the actual file you are uploading.
-        media_body=MediaFileUpload("test.mp4", resumable=True)
+        # The chunksize parameter specifies the size of each chunk of data, in
+        # bytes, that will be uploaded at a time. Set a higher value for
+        # reliable connections as fewer chunks lead to faster uploads. Set a lower
+        # value for better recovery on less reliable connections.
+        #
+        # Setting "chunksize" equal to -1 in the code below means that the entire
+        # file will be uploaded in a single HTTP request. (If the upload fails,
+        # it will still be retried where it left off.) This is usually a best
+        # practice, but if you're using Python older than 2.6 or if you're
+        # running on App Engine, you should set the chunksize to something like
+        # 1024 * 1024 (1 megabyte).
+        media_body=MediaFileUpload(file, chunksize=-1, resumable=True)
     )
+    return request
+
+def upload(request):
+    response = None
 
     while response is None:
         status, response = request.next_chunk()
@@ -64,6 +70,17 @@ def main():
     
     print('Upload complete')
 
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    credentials = get_credentials(CLIENT_SECRETS_FILE, API_SERVICE_SCOPES)
+    api_client = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    request = get_youtube_video_insert_request(api_client, FILE, "Automated file upload", FILE)
+    upload(request)
 
 if __name__ == "__main__":
     main()
