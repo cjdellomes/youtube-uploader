d71ef6201bcddf5a2916f4698c449451813a2e9a
cjdellomes <cjdellomes@gmail.com>
2020-11-06 19:03:33 -0800

    Create simple html page for Google OAuth
    Our uploader currently uses the command line for oauth authorization.
    This authentication is needed to upload videos to youtube channels. We
    want to be able to do authorization on the client side then pass the
    access token through the controller to the uploader. Use the gapi
    javascript client to allow for this client side authorization.

--- controller.py
+++ controller.py
@@ -15,15 +15,7 @@ def is_allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload():
     if request.method != 'POST':
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-            <input type=file name=file>
-            <input type=submit value=Upload>
-        </form>
-        '''
+        return send_from_directory('/home/cjdellomes/youtube-uploader', 'example.html')
 
     if 'file' not in request.files:
         flash('No file part')
